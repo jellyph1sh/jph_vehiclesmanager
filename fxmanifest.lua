c41faebff534a0806b3071d5e25525da4f09f7b2
JellyPh1sh <sacha.sorgiati@gmail.com>
2023-05-01 06:52:56 +0000

    :rocket: | Deploy resource base.

---
+++ fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version "cerulean"
+game "gta5"
+
+version "1.0"
+description "Spawn/Delete/Repair any vehicles with commands."
+author "Jellyph1sh"
+
+client_script "cl_vehiclesmanager.lua"
+
+shared_config "config.lua"
